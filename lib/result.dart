31dcec3c74e99046d2f853526f7052f8204ff0b0
PeterHChang <79433622+PeterHChang@users.noreply.github.com>
2021-03-28 22:04:34 -0500

    Add files via upload

---
+++ lib/result.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetHandler;
+
+  Result(this.resultScore, this.resetHandler);
+
+  //Remark Logic
+  String get resultPhrase {
+    String resultText;
+    if (resultScore >= 95) {
+      resultText = 'You suffer from a very severe social anxiety';
+      print(resultScore);
+    } else if (resultScore >= 80 && resultScore <= 94) {
+      resultText = 'You suffer from a severe social anxiety';
+      print(resultScore);
+    } else if (resultScore >= 65 && resultScore <= 79) {
+      resultText = 'You suffer from a marked social anxiety';
+      print(resultScore);
+    } else if (resultScore >= 50 && resultScore <= 64) {
+      resultText = 'You suffer from a moderate social anxiety';
+      print(resultScore);
+    } else if (resultScore >= 30 && resultScore <= 49) {
+      resultText = 'You suffer from a mild social anxiety';
+      print(resultScore);
+    } else if (resultScore >= 0 && resultScore <= 29) {
+      resultText = 'You do not suffer from social anxiety';
+      print(resultScore);
+    }
+    return resultText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ), //Text
+          Text(
+            'Score ' '$resultScore',
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ), //Text
+          // ignore: deprecated_member_use
+          FlatButton(
+            child: Text(
+              'Restart Quiz!',
+            ), //Text
+            textColor: Colors.blue,
+            onPressed: resetHandler,
+          ), //FlatButton
+        ], //<Widget>[]
+      ), //Column
+    ); //Center
+  }
+}
